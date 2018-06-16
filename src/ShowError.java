252b5b8cfaa1454d387fada8ab43715f9a07dbae
Hugo Gustavo <hugog.valin@gmail.com>
2018-06-16 19:30:46 -0300

    Aprendendo Servlet Mostrando Erro

---
+++ src/ShowError.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ShowError")
+public class ShowError extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set error code and reasson.
+		response.sendError(407, "Need Authentication!!!");
+
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
